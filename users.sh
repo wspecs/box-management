312206dbeead1f192e9046f7e8f0e55555dc2947
Wendly Saintil <wspecs@hotmail.com>
2020-03-18 15:10:52 -0700

    Update users.sh

--- users.sh
+++ users.sh
@@ -24,12 +24,12 @@ if [ -z "`tools/mail.py user`" ]; then
   # creating admin users
   EMAIL_ADDR=wspecs@$HOST
   ADMIN_USER_SECRET=$(openssl rand -base64 48 | tr -d "=+/" | cut -c1-64)
-  add_config USER_SECRET=$USER_SECRET $HOME/.wspecsbox.conf
+  add_config ADMIN_USER_SECRET=$ADMIN_USER_SECRET $HOME/.wspecsbox.conf
   echo
   echo "Creating a new administrative mail account for $EMAIL_ADDR."
 
   # Create the user's mail account. This will ask for a password if none was given above.
-  tools/mail.py user add $EMAIL_ADDR ${USER_SECRET:-}
+  tools/mail.py user add $EMAIL_ADDR ${ADMIN_USER_SECRET:-}
 
   # Make it an admin.
   hide_output tools/mail.py user make-admin $EMAIL_ADDR
